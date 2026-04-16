989d052e4f229f655fcb64b233f544b766aa4f0c
Asad-bot07 <asadhussain2408@gmail.com>
2026-04-17 00:57:56 +0530

    refactor: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,30 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import AdminRouteGate from './admin/component/AdminRouteGate.tsx'
+import AdminLoginPage from './admin/pages/AdminLoginPage.tsx'
+import AdminDashboardPage from './admin/pages/AdminDashboardPage.tsx'
 import ProjectDetailsPage from './pages/ProjectDetailsPage.tsx'
+import AllProjectsPage from './pages/AllProjectsPage.tsx'
+import AllBlogsPage from './pages/AllBlogsPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/projects" element={<AllProjectsPage />} />
+        <Route path="/blogs" element={<AllBlogsPage />} />
         <Route path="/projects/:slug" element={<ProjectDetailsPage />} />
+        <Route path="/v3/admin/login-page" element={<AdminLoginPage />} />
+        <Route
+          path="/v4/admin/dashboard"
+          element={
+            <AdminRouteGate>
+              <AdminDashboardPage />
+            </AdminRouteGate>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
